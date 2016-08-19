a0a38c7a1851112f35345005c1e6b166d7d035a5
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-08-19 11:04:37 -0700

    adds hide/show to edit

--- app/components/q-details.js
+++ app/components/q-details.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showUpdate: false,
   actions:{
+    showUpdate() {
+      this.set("showUpdate", true);
+    },
+
+    hideUpdate() {
+      this.set("showUpdate", false);
+    },
+
     update(q, params) {
+      this.set("showUpdate", false);
       this.sendAction('update', q, params);
     }
   }
